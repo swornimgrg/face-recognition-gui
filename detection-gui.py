4e091c8ec1bc61a8e4b79cc745f8f421af9aca78
swornimgrg <31173400+swornimgrg@users.noreply.github.com>
2019-07-23 23:39:18 +0545

    Add files via upload

---
+++ detection-gui.py
@@ -0,0 +1,72 @@
+import os
+import numpy
+import cv2 
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+
+
+window = tk.Tk()
+window.title("Face Recognition")
+window.geometry('600x400')
+
+
+head = Label(window, text= "Face Detection & Recognition", width=30, fg= "blue", font=" Georgia 14 bold")
+head.place(x=20, y=20)
+#head.grid(row=0,column=0)
+
+path= 'C:\\Users\\Swornim\\Face_detection\\Images\\default.jpg'
+
+photo = ImageTk.PhotoImage(Image.open(path))
+panel = tk.Label(window, image=photo)
+panel.place(x=350,y=70)
+#panel.grid(row=0, column=1)
+
+def load_photo(file_path):
+	image = image_path("file_path")
+	imgLabel.configure(image=image)
+	imageLabel.image= image
+
+def browse_image():
+	global image_file_name
+	global image_selected 
+	global file_to_copy
+
+	path_to_image = filedialog.askopenfilename(initialdir = "C:\\Users\\Swornim\\Face_detection\\Images",title = "Select file",filetypes = (("jpeg files","*.jpg"),("jpeg files","*.jpeg*")))
+	
+	try:
+		if path_to_image:
+			image_file_name = os.path.basename(path_to_image)
+			file_to_copy = path_to_image
+			image_selected = True
+			load_photo(file_to_copy)
+	except IOError as err:
+		image_selected = False
+		messagebox.showinfo("File Error",err)
+
+
+ 
+
+#photo = tk.PhotoImage(file="C:\\Users\\Swornim\\Face_detection\\Images")
+
+button1= tk.Button(window, text="Select Image", width=20, command=browse_image)
+button1.place(x=60 , y=70)
+
+button2= tk.Button(window, text="Add Image to Dataset",width=20)
+button2.place(x=60 , y=115)
+
+button3= tk.Button(window, text="Database Info",width=20)
+button3.place(x=60 , y=160)
+
+button4= tk.Button(window, text="Face Recognition",width=20)
+button4.place(x=60 , y=205)
+
+button5= tk.Button(window, text="Delete Image",width=20)
+button5.place(x=60 , y=250)
+
+
+
+window.mainloop()
+
+
