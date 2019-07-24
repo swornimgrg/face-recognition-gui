8d4c598bae55baa3b776e1332a4d06da0c242fc5
swornimgrg <31173400+swornimgrg@users.noreply.github.com>
2019-07-24 10:58:37 +0545

    Add files via upload

--- detection-gui.py
+++ detection-gui.py
@@ -15,42 +15,30 @@ window.geometry('600x400')
 
 head = Label(window, text= "Face Detection & Recognition", width=30, fg= "blue", font=" Georgia 14 bold")
 head.place(x=20, y=20)
-#head.grid(row=0,column=0)
 
 path = 'C:\\Users\\Swornim\\Face_detection\\Images\\default.jpg'
 
 photo = ImageTk.PhotoImage(Image.open(path))
 panel = tk.Label(window, image=photo)
 panel.place(x=350,y=70)
-#panel.grid(row=0, column=1)
 
 def browse_image():
-	global image_file_name
-	global image_selected 
-	global file_to_copy
-	global photo1
+	global image_selected
+	global photo
 
 	path_to_image = filedialog.askopenfilename(initialdir = "C:\\Users\\Swornim\\Face_detection\\Images\\yalefaces",title = "Select file",filetypes = (("jpeg files","*.jpg"),("jpeg files","*.jpeg*")))
 	
 	try:
 		if path_to_image:
-			#path1 = 'C:\\Users\\Swornim\\Face_detection\\Images\\default.jpg'
-			photo1 = ImageTk.PhotoImage(Image.open(path_to_image))
+			photo = ImageTk.PhotoImage(Image.open(path_to_image))
 			print(path_to_image)
-			panel1 = tk.Label(window, image=photo1)
-			panel1.place(x= 350,y= 70)
-			#image_file_name = os.path.(path_to_image)
-			#file_to_copy = path_to_image
+			panel1 = tk.Label(window, image=photo)
+			panel1.place(x= 250,y= 70)
 			image_selected = True
 
 	except IOError as err:
 		image_selected = False
 		messagebox.showinfo("File Error",err)
-
-
- 
-
-#photo = tk.PhotoImage(file="C:\\Users\\Swornim\\Face_detection\\Images")
 
 button1= tk.Button(window, text="Select Image", width=20, command=browse_image)
 button1.place(x=60 , y=70)
@@ -66,8 +54,6 @@ button4.place(x=60 , y=205)
 
 button5= tk.Button(window, text="Delete Image",width=20)
 button5.place(x=60 , y=250)
-
-
 
 window.mainloop()
 
