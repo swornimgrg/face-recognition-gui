233110f21d370209df354b82eb9c7859f8712bbf
swornimgrg <31173400+swornimgrg@users.noreply.github.com>
2019-07-25 21:04:34 +0545

    Add files via upload

--- detection-gui.py
+++ detection-gui.py
@@ -6,11 +6,16 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 from PIL import ImageTk, Image
+import matplotlib.image as mpimg
 
 global panel
 window = tk.Tk()
 window.title("Face Recognition")
-window.geometry('600x400')
+#window.geometry('600x400')
+canvas_width = 600
+canvas_height = 400
+canvas = Canvas(window, width = canvas_width, height = canvas_height)
+canvas.pack()
 
 
 head = Label(window, text= "Face Detection & Recognition", width=30, fg= "blue", font=" Georgia 14 bold")
@@ -32,44 +37,43 @@ def browse_image():
 	
 	try:
 		if path_to_image:
+			#arr1 = np.zeros((233,220), dtype= np.uint8)
+			img = np.array(Image.open(path_to_image))
 			photo = ImageTk.PhotoImage(Image.open(path_to_image))
+			#photo = photo.astype(np.uint8)
 			print(path_to_image)
-			panel = tk.Label(window, image=photo)
-			panel.place(x= 250,y= 70)
+			panel = tk.Label(window, image = photo)
+			panel.image = photo
+			panel.place(x = 250,y = 70)
 			image_selected = True
 
 	except IOError as err:
 		image_selected = False
 		messagebox.showinfo("File Error",err)
 
-
+	print(img.shape)
 
 def detect_face():
-
-	face_cascade= cv2.CascadeClassifier("C:\\Python\\Python37\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
-
-	img = cv2.imread(path_to_image)
-
+	global img
+	face_cascade = cv2.CascadeClassifier("C:\\Python\\Python37\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
 	faces = face_cascade.detectMultiScale(img, scaleFactor = 1.16, minNeighbors = 5)
+
 	print(faces)
 
 	for x,y,w,h in faces:
-		img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0),2)
+		img = rectangle(img, (x,y), (x+w, y+h), (0,255,0), 10)
+	
 
-	#photo = ImageTk.PhotoImage(Image.open(path_to_image))
-
-
-
-button1= tk.Button(window, text="Browse", width=20, command=browse_image)
+button1= tk.Button(window, text = "Browse", width = 20, command = browse_image)
 button1.place(x=60 , y=70)
 
-button2= tk.Button(window, text="Add Image to Dataset",width=20)
+button2= tk.Button(window, text = "Add Image to Dataset",width = 20)
 button2.place(x=60 , y=115)
 
-button3= tk.Button(window, text="Database Info",width=20)
+button3= tk.Button(window, text = "Database Info",width=20)
 button3.place(x=60 , y=160)
 
-button4= tk.Button(window, text="Face Recognition",width=20,command=detect_face)
+button4= tk.Button(window, text = "Face Recognition",width=20 , command = detect_face)
 button4.place(x=60 , y=205)
 
 button5= tk.Button(window, text="Delete Image",width=20)
