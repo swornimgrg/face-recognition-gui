8a3ccd7090b9d8246e85fd9a6993db3563db8748
swornimgrg <31173400+swornimgrg@users.noreply.github.com>
2019-07-26 00:12:32 +0545

    Add files via upload

--- detection-gui.py
+++ detection-gui.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import cv2 
 import tkinter as tk
@@ -6,15 +5,16 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 from PIL import ImageTk, Image
-import matplotlib.image as mpimg
 
-global panel
+
+global canvas
+
 window = tk.Tk()
 window.title("Face Recognition")
-#window.geometry('600x400')
+window.geometry('600x400')
 canvas_width = 600
 canvas_height = 400
-canvas = Canvas(window, width = canvas_width, height = canvas_height)
+canvas = Canvas(window, width = canvas_width, height = canvas_height, relief = 'raised')
 canvas.pack()
 
 
@@ -24,8 +24,8 @@ head.place(x=20, y=20)
 path = 'C:\\Users\\Swornim\\Face_detection\\Images\\default.jpg'
 
 photo = ImageTk.PhotoImage(Image.open(path))
-panel = tk.Label(window, image=photo)
-panel.place(x=350,y=70)
+
+canvas.create_image(250,70, anchor=NW, image=photo)
 
 def browse_image():
 	global image_selected
@@ -37,14 +37,10 @@ def browse_image():
 	
 	try:
 		if path_to_image:
-			#arr1 = np.zeros((233,220), dtype= np.uint8)
 			img = np.array(Image.open(path_to_image))
 			photo = ImageTk.PhotoImage(Image.open(path_to_image))
-			#photo = photo.astype(np.uint8)
 			print(path_to_image)
-			panel = tk.Label(window, image = photo)
-			panel.image = photo
-			panel.place(x = 250,y = 70)
+			canvas.create_image(250,70, anchor=NW, image=photo)
 			image_selected = True
 
 	except IOError as err:
@@ -61,7 +57,7 @@ def detect_face():
 	print(faces)
 
 	for x,y,w,h in faces:
-		img = rectangle(img, (x,y), (x+w, y+h), (0,255,0), 10)
+		canvas.create_rectangle(x+250, y+70, w+x+250, h+y+70, outline = '#39ff14' , width = 2)
 	
 
 button1= tk.Button(window, text = "Browse", width = 20, command = browse_image)
